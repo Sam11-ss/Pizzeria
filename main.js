1c2f4dd20d72c111e0e0d8c0dbbc2164c95f71c5
Sam11-ss <84765714+Sam11-ss@users.noreply.github.com>
2021-08-28 12:56:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-menu_list_array = ["Veg Margherita Pizza","Chicken Tandoori Pizza","Chicken kabab pizza","Vegetable Pizza", "Paneer Pizza","King size Chicken pizza"
-                    ];
+menu_list_array = ["Margharita Pizza","Chicken Kebab Pizza","Veg Deluxe Pizza", "Barbecue Chicken Pizza","Mushroom Pizza"];
 
 function getmenu(){
-var htmldata;
+var htmldata="";
 menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+ menu_list_array[i] + '<br>'
@@ -12,13 +11,14 @@ menu_list_array.sort();
 }
 
 function add_item(){
-var htmldata;
+var htmldata="";
+
 var item=document.getElementById("add_item").value;
  menu_list_array.sort();
         
-        htmldata=""
+        htmldata="";
         for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+            htmldata=htmldata+menu_list_array[i]+'<br>';
         }
          document.getElementById("display_addedmenu").innerHTML = htmldata;		
       }
